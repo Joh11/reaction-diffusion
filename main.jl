6baff0aad5a2ea957b5b108dec8d0afa9e9a26a7
Johan Felisaz <johan.felisaz@epfl.ch>
2024-06-13 17:09:45 +0200

    update stuff

--- main.jl
+++ main.jl
@@ -1,70 +1,144 @@
 using SimpleDirectMediaLayer
 const SDL2 = SimpleDirectMediaLayer
 using ColorTypes
+using DelimitedFiles
 
-@kwdef struct Parameters
+abstract type Parameters end
+
+function scalingparameter(p::Parameters)
+    dt(p) / dx(p)^2 * D(p)
+end
+
+@kwdef struct GrayScott <: Parameters
     dt :: Float64
     dx :: Float64
-    D :: Float64
+    
+    k :: Float64 = 0.062
+    f :: Float64 = 0.055
+    Du :: Float64 = 1
+    Dv :: Float64 = 0.5
 end
 
-function scalingparameter(params::Parameters)
-    params.D * params.dt / params.dx^2
-end
+function dt(gs::GrayScott) gs.dt end
+function dx(gs::GrayScott) gs.dx end
+function D(gs::GrayScott) return [gs.Du, gs.Dv] end
 
-abstract type Reaction end
+# function initρ(w, h)
+#     ρ = zeros(w, h, 2)
+#     r = 5
+#     ρ[w÷2-r:w÷2+r, h÷2-r:h÷2+r, 1] .= 0.7
+#     r = 10
+#     ρ[w÷2-r:w÷2+r, h÷2-r:h÷2+r, 2] .= 0.3
+#     ρ
+# end
 
-@kwdef struct GrayScott <: Reaction
-    k :: Float64 = 0.06264
-    f :: Float64 = 0.06100
-end
+# function initρ(w, h)
+#     ρ = zeros(w, h, 2)
+#     ρ[:, :, 1] .= 1
+#     r = 5
+#     ρ[w÷2-r:w÷2+r, h÷2-r:h÷2+r, 2] .= 1
+#     ρ
+# end
 
 function initρ(w, h)
-    # only a single channel for now
-    ρ = zeros(w, h, 3)
-    r = 5
-    ρ[w÷2-r:w÷2+r, h÷2-r:h÷2+r, 1] .= 0.7
-    r = 10
-    ρ[w÷2-r:w÷2+r, h÷2-r:h÷2+r, 2] .= 0.3
+    @assert w == h
+    
+    x = repeat(0:w-1, 1, h) .- w/2
+    y = repeat(0:h-1, 1, w)' .- h/2
+    r = .√(x.^2 + y.^2)
+    
+    ρ = zeros(w, h, 2)
+    ρ[:, :, 1] .= 1
+
+    r0 = .4 * (w + h) / 2
+    
+    @views reshape(ρ[:, :, 2], :)[reshape(r, :) .< r0] .= 1
+    
     ρ
 end
 
-function diffusionpart(params::Parameters, ρ)
-    dρ = -4ρ + circshift(ρ, [1, 0]) + circshift(ρ, [-1, 0]) + circshift(ρ, [0, 1]) + circshift(ρ, [0, -1])
+# function initρ(w, h)
+#     ρ = zeros(w, h, 2)
+#     ρ[1:end, 1:end, 1:2] = rand(w, h, 2)
+#     ρ
+# end
 
-    scalingparameter(params) * dρ
+function diffusion(p, ρ)
+    dρ = -4ρ +
+        0.8 * (circshift(ρ, [1, 0]) + circshift(ρ, [-1, 0]) +
+        circshift(ρ, [0, 1]) + circshift(ρ, [0, -1])) +
+        0.2 * (circshift(ρ, [1, 1]) + circshift(ρ, [-1, 1]) +
+        circshift(ρ, [1, -1]) + circshift(ρ, [-1, -1]))
+    
+    reshape(scalingparameter(p), 1, 1, :) .* dρ
 end
 
-function update(params::Parameters, ρ, nothing::Nothing)
-    ρ + diffusionpart(params, ρ)
-end
+# function update(p::GrayScott, ρ)
+#     u = ρ[:, :, 1]
+#     v = ρ[:, :, 2]
 
-function update(params::Parameters, ρ, gs::GrayScott)
+#     du = (-u .* v.^2 + p.f * (1 .- u)) * dt(p)
+#     dv = (u .* v.^2 - (p.f + p.k) * v) * dt(p)
+
+#     ρ2 = ρ + diffusion(p, ρ) + stack((du, dv))
+#     ρ2
+# end
+
+# the one with extent
+# function update(p::GrayScott, ρ)
+#     u = ρ[:, :, 1]
+#     v = ρ[:, :, 2]
+
+#     x = u .* v.^2 .* (1 .- v).^3
+#     du = (-x + p.f * (1 .- u)) * dt(p)
+#     dv = (x - (p.f + p.k) * v) * dt(p)
+
+#     ρ2 = ρ + diffusion(p, ρ) + stack((du, dv))
+#     ρ2
+# end
+
+# with disorder
+function update(p::GrayScott, ρ)
+    # α = 2e-5
+    α = 1e-5 # disorder
     u = ρ[:, :, 1]
     v = ρ[:, :, 2]
 
-    du = (-u .* v.^2 + gs.f * (1 .- u)) * params.dt
-    dv = (u .* v.^2 - (gs.f + gs.k) * v) * params.dt
+    du = (-u .* v.^2 + p.f * (1 .- u)) * dt(p)
+    dv = (u .* v.^2 - (p.f + p.k) * v) * dt(p)
 
-    ρ + diffusionpart(params, ρ) + stack((du, dv, zeros(size(du))))
+    ρ + diffusion(p, ρ) + stack((du, dv)) + √(α * dt(p)) * randn(size(ρ))
 end
 
 function datafromρ(ρ)
-    ARGB32.(clamp.(ρ[:, :, 1], 0, 1), clamp.(ρ[:, :, 2], 0, 1), clamp.(ρ[:, :, 3], 0, 1), 1)
+    nchannels = size(ρ, 3)
+    
+    if nchannels == 1
+        ARGB32.(clamp.(ρ[:, :, 1], 0, 1), 0, 0, 1)
+    elseif nchannels == 2
+        ARGB32.(clamp.(ρ[:, :, 1], 0, 1), clamp.(ρ[:, :, 2], 0, 1), 0, 1)
+    elseif nchannels >= 3
+        ARGB32.(clamp.(ρ[:, :, 1], 0, 1), clamp.(ρ[:, :, 2], 0, 1),
+                clamp.(ρ[:, :, 3], 0, 1), 1)
+    end
 end
 
 @assert SDL2.init() == 0
 
 # logical size
-w = Int32(100)
-h = Int32(100)
+w = Int32(400)
+h = Int32(400)
 
-params = Parameters(dt=1, dx=1, D=0.2)
+# this one is found to work
+# p = GrayScott(dt=1, dx=5, Du=5, Dv=5/2)
+
+p = GrayScott(dt=1, dx=5, Du=5, Dv=5/2)
+
 println("Parameters:")
-@show params
-@show scalingparameter(params)
+@show p
+@show scalingparameter(p)
 
-reaction = GrayScott()
+stepspertick = 30
 
 try
     @show win = SDL2.CreateWindow("Reaction diffusion", Int32(SDL2.WINDOWPOS_CENTERED()), Int32(SDL2.WINDOWPOS_CENTERED()), w, h, SDL2.WINDOW_RESIZABLE)
@@ -98,11 +172,25 @@ try
             if event isa SDL2.QuitEvent
                 @show event
                 quit = true
+            elseif event isa SDL2.KeyboardEvent && event.repeat == 0
+                if event.keysym.sym == SDL2.GetKeyFromName("r")
+                    println("reset")
+                    ρ = initρ(w, h)
+                elseif event.keysym.sym == SDL2.GetKeyFromName("s")
+                    println("save")
+                    writedlm("rho.dat", ρ)
+                elseif event.keysym.sym == SDL2.GetKeyFromName("l")
+                    println("load")
+                    ρ = reshape(readdlm("rho.dat"), Int(w), Int(h), :)
+                end
             end
             event = SDL2.event()
         end
 
-        ρ = update(params, ρ, reaction)
+        for n in 1:stepspertick
+            ρ = update(p, ρ)
+        end
+
         data .= datafromρ(ρ)
 
         SDL2.RenderClear(render)
@@ -112,9 +200,9 @@ try
         
         SDL2.RenderPresent(render)
         
-        SDL2.Delay(UInt32(30))
+        # SDL2.Delay(UInt32(30))
 
-        if tick % 10 == 0
+        if tick % 1 == 0
             println(sum(ρ))
         end
         tick += 1
